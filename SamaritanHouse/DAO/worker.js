b09e641bbe17ddf04f6992a3c6608318481d58da
Saideepak Budaraju <saideepak09@gmail.com>
2015-10-04 04:18:39 -0700

    Worker APIs

--- SamaritanHouse/DAO/worker.js
+++ SamaritanHouse/DAO/worker.js
@@ -1,8 +1,8 @@
 var mysql = require('./dbConnectionsController');
 
-exports.getWorkerDetails = function(json) {	
+exports.getWorkerDetails = function(WorkerID) {	
 		var connection=mysql.getConnection();
-		var query = connection.query("select * from WorkerInfo ",
+		var query = connection.query("select * from WorkerInfo where WorkerID ? ",WorkerID ,
 				function(err, rows) {
 			connection.end();
 			if (err) {
@@ -15,12 +15,13 @@ exports.getWorkerDetails = function(json) {
 		});
 };	
 
-exports.newWorker = function (callback, json) {	
+exports.newWorker = function (callback, res, json, skID) {	
 	var connection=mysql.getConnection();
 	
 	var query = connection.query("INSERT INTO WorkerInfo set ? ",json, function(err, r){
 		if (err) {
-			console.log("Error Here: " + r);									
+			console.log("Error: " + r);		
+			connection.end();								
 		} 
 		else {		
 			console.log("Level 1");
@@ -30,24 +31,53 @@ exports.newWorker = function (callback, json) {
 			conn.query("select MAX(WorkerID) as id from WorkerInfo", function(error, results){
 				if(error){
 					console.log("Sleep!!! "+error);
+					conn.end();	
 				}else{
-					console.log("Level 2");
-					connection.query("INSERT INTO UserSkills set ? ", {WorkerID: json.SkillID, SkillID: json.SkillID} , function(errors, rows){
+					console.log("Level 2: "+results[0].id);
+					conn.query("INSERT INTO UserSkills set ? ", {WorkerID: results[0].id, SkillID: skID} , function(errors, rows){
 						if (errors) {
 							console.log("sdsd "+ errors);
+							conn.end();	
 						} 
 						else {
-							console.log("Level 3");
+							console.log("Level 3 SUCCESS");
 							res.send({"msg":"successfully inserted"});
-							// connection.end();	
+							connection.end();	
 						}
-					// });
 					});
 				}
-		//	console.log(json);
 			});
-		connection.end();	
 		}
 	});
+
+}
+
+exports.editWorker = function(callback, json){
+	var connection=mysql.getConnection();
+
+	//Change Query.
+	var query = connection.query("UPDATE WorkerInfo set ? where WorkerID= ? ",[json,json.WorkerID], function(err, r){
+		if (err) {
+			console.log("Error: " + r);		
+			connection.end();								
+		} 
+		else {		
+			console.log("Level 1");
+		}
+	});	
 }
  
+exports.deleteWorker = function(callback, WorkerID){
+	var connection=mysql.getConnection();
+
+	//Change Query.
+	var query = connection.query("DELETE from WorkerInfo where WorkerID=? ",WorkerID, function(err, r){
+		if (err) {
+			console.log("Error: " + r);		
+			connection.end();								
+		} 
+		else {		
+			console.log("Level 1");
+		}
+	});	
+}
